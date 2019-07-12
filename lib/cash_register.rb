e229f6e94853f2966f0f6e6bbea683aaf50338c4
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 15:43:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
 attr_accessor :total
 
-def initialize(total = 100)
+def initialize(total = 0)
   @total = total
 end
 
@@ -15,7 +15,7 @@ def add_item(title,price,count=1)
 end
 
 def apply_discount
-  
+
 
 end
 end
