235464841c7dcb2d2b92930a18c95dd2cf25d433
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 16:15:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,11 +8,11 @@ end
 
 def discount
    @total = @total - (self.total*(20/100))
-   
+
 end
 
-def add_item(title,price,count=1)
-  @total=self.total+(price*count)
+def add_item(title,price,count=1) #80 + @18
+  self.total+(price*count)
 end
 
 def apply_discount
