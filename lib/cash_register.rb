b2911f0704f7c278869760c2deffd7b897de2df9
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 17:35:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,8 @@ attr_accessor :discount, :item, :last_transaction, :total
   end
 
 
-def add_item(title,price,count=1) #80 + @18
+def add_item(title,price,count=1)
+  @items << title#80 + @18
   @total = self.total+(price*count)
 end
 
