9be09b77ece72487c3de69fa57fb26497b8caa20
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 17:10:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,17 +9,19 @@ attr_accessor :discount, :item, :last_transaction, :total
     @total = 0
   end
 
-def discount
-     @discount = @total*(20/100)
-     @total = @total-@discount
-     @total
-end
 
 def add_item(title,price,count=1) #80 + @18
   @total = self.total+(price*count)
 end
 
 def apply_discount
-
+      if @discount > 0
+        @discount = @discount/100.to_f
+        @total = @total - (@total * (@discount))
+        "After the discount, the total comes to $#{@total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+    end
 end
 end
